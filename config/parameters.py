7b699d122942b29de85018b64af2726b76235731
Osama <oabuoun1>
2017-12-08 10:08:03 +0000

    chainging redis server to 127.0.0.1

--- config/parameters.py
+++ config/parameters.py
@@ -17,8 +17,8 @@ def broker():
 	return broker
 
 backend_protocol 			= 	'redis'
-#backend_server	 			=	'192.168.253.1'
-backend_server	 			=	'redis'
+backend_server	 			=	'127.0.0.1'
+#backend_server	 			=	'redis'
 backend_port     			=	16379
 backend_db		 			=	0
 backend_experiment_db_id	=	10
